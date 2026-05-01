4f41074a6fe8f5adf14186b3e1fc6e5aa52adddf
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-05-01 17:41:39 +0200

    Section 22 - Spring Boot Actuator - Application Monitoring

--- src/main/java/net/javaguides/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/net/javaguides/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -1,6 +1,8 @@
 package net.javaguides.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -19,4 +21,8 @@ public class PhotoAppApiUsersApplication {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
--- src/main/java/net/javaguides/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/net/javaguides/photoapp/api/users/security/WebSecurity.java
@@ -5,6 +5,7 @@ import net.javaguides.photoapp.api.users.service.UsersService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -58,7 +59,7 @@ public class WebSecurity {
                                 + environment.getProperty("gateway.ip") + "')"))
                         .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("/users/status/check").permitAll()
-                        //.anyRequest().authenticated()
+                        .requestMatchers("/actuator/**").permitAll()
                 )
                 .addFilter(authenticationFilter)
                 .authenticationManager(authenticationManager)
