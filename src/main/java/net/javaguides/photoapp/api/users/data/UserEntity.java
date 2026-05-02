35c1f0f196d5fb1798a20c6102bac2e2f5fbf06d
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-05-02 10:38:41 +0200

    Section 23: Using MySQL Instead of In-Memory Database

--- src/main/java/net/javaguides/photoapp/api/users/data/UserEntity.java
+++ src/main/java/net/javaguides/photoapp/api/users/data/UserEntity.java
@@ -3,6 +3,7 @@ package net.javaguides.photoapp.api.users.data;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
@@ -29,7 +30,7 @@ public class UserEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 50)
